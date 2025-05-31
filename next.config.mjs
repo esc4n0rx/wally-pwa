1ac12bf74e315b24861398e7d5c09fc6f76d8774
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-05-31 20:39:50 -0300

    fIXES

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,4 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
@@ -46,4 +47,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig; 
